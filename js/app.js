779d203b5b483edd7aa24771b7343c2d906c555d
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2021-01-03 21:11:23 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('this is a test')
+const navSlide = () => {
+  const burger = document.querySelector('#burger');
+  const mainNav = document.querySelector('#main-nav');
+
+  burger.addEventListener('click', () => {
+    console.log('i was clicked');
+    mainNav.classList.toggle('nav-active');
+    burger.classList.toggle('burger-move');
+  })
+}
+
+navSlide()
